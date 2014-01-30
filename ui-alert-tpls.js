d242c18b2f0e05d75e3b12fb79fbefbbdacd4ddc
Andy Joslin <andyjoslin@ajoslin-3.local>
2014-01-30 14:53:44 -0500

    chore(release): 0.11.0-SNAPSHOT

---
+++ ui-alert-tpls.js
@@ -0,0 +1,33 @@
+/*
+ * angular-ui-bootstrap
+ * http://angular-ui.github.io/bootstrap/
+ * Version: 0.11.0-SNAPSHOT - 2014-01-30
+ * License: MIT
+ */
+
+
+
+angular.module('ui.bootstrap.alert', [])
+
+.controller('AlertController', ['$scope', '$attrs', function ($scope, $attrs) {
+  $scope.closeable = 'close' in $attrs;
+}])
+
+.directive('alert', function () {
+  return {
+    restrict:'EA',
+    controller:'AlertController',
+    templateUrl:'template/alert/alert.html',
+    transclude:true,
+    replace:true,
+    scope: {
+      type: '@',
+      close: '&'
+    }
+  };
+});
+
+angular.module('template/alert/alert.html', []).run(['$templateCache', function($templateCache) {
+  $templateCache.put('template/alert/alert.html',
+    '<div class="alert" ng-class=""alert-" + (type || "warning")"><button ng-show="closeable" type="button" class="close" ng-click="close()">&times;</button><div ng-transclude=""></div></div>');
+}]);
