5089b48787dcb72a387092637bbefcd4a32d806b
Andy Joslin <andytjoslin@gmail.com>
2014-01-31 10:44:57 -0500

    chore(release):0.11.0

---
+++ ui-alert.js
@@ -0,0 +1,27 @@
+/*
+ * angular-ui-bootstrap
+ * http://angular-ui.github.io/bootstrap/
+ * Version: 0.11.0 - 2014-01-31
+ * License: MIT
+ */
+
+
+angular.module('ui.bootstrap.alert', [])
+
+.controller('AlertController', ['$scope', '$attrs', function ($scope, $attrs) {
+  $scope.closeable = 'close' in $attrs;
+}])
+
+.directive('alert', function () {
+  return {
+    restrict:'EA',
+    controller:'AlertController',
+    templateUrl:'template/alert/alert.html',
+    transclude:true,
+    replace:true,
+    scope: {
+      type: '@',
+      close: '&'
+    }
+  };
+});
